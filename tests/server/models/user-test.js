a403a48d7f0ecdfea0e3efbf1958e517a69eb296
Jason Hang <dev.jdhang@gmail.com>
2016-06-06 13:52:58 -0400

    Changed the database name to pandoras-kits

--- tests/server/models/user-test.js
+++ tests/server/models/user-test.js
@@ -2,7 +2,7 @@ var sinon = require('sinon');
 var expect = require('chai').expect;
 
 var Sequelize = require('sequelize');
-var dbURI = 'postgres://localhost:5432/testing-fsg';
+var dbURI = 'postgres://localhost:5432/testing-pandoras-kits';
 var db = new Sequelize(dbURI, {
     logging: false
 });
