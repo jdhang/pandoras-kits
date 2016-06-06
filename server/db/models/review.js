e7ec05e08bcc2a3877777af268ba8ec30adacc6f
Angel <rriera96@gmail.com>
2016-06-06 15:51:12 -0400

    added review.js and review model logic

---
+++ server/db/models/review.js
@@ -0,0 +1,27 @@
+'use strict';
+var Sequelize = require('sequelize');
+
+module.exports = function (db) {
+
+    db.define('review', {
+        num_stars: {
+            type: Sequelize.Integer,
+            validate:{ min: 1, max: 5}
+        },
+        text: {
+            type: Sequelize.TEXT
+        }
+    }, {
+        instanceMethods: {
+        },
+        classMethods: {
+        },
+        hooks: {
+            beforeValidate: function (kit) {
+                //if (kit) {
+                    //kit.change
+                //}
+            }
+        }
+    });
+};
