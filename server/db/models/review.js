b2fac0a6c3b799c5205790ec766120a5acb1e040
Angel <rriera96@gmail.com>
2016-06-07 11:53:04 -0400

    removed extra semi-colon from review.js

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -22,7 +22,7 @@ module.exports = function (db) {
             findByUser: function(userId){
                 return this.findAll({
                     where: {
-                       userId: userId;
+                       userId: userId
                     }
                 });
             }
