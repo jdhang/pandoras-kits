b0e70d9c96264466884a7313e706b90fc1940750
Jason Hang <dev.jdhang@gmail.com>
2016-06-13 14:11:45 -0400

    Adding functionality where user can edit their existing review on a kit.

--- browser/js/reviews/review-item.directive.js
+++ browser/js/reviews/review-item.directive.js
@@ -1,11 +1,30 @@
 'use strict'
 
-app.directive('reviewItem', () => {
+app.directive('reviewItem', (AuthService, $uibModal) => {
 
   return {
     restrict: 'E',
     scope: { review: '=' },
-    templateUrl: 'js/reviews/templates/review-item.html'
+    templateUrl: 'js/reviews/templates/review-item.html',
+    link: (scope) => {
+
+      AuthService.getLoggedInUser()
+      .then(user => {
+        scope.user = user
+        c
+        
+      })
+
+      scope.open = (size) => {
+        $uibModal.open({
+          templateUrl: '/js/reviews/templates/review-form.html',
+          controller: 'ReviewFormCtrl',
+          scope: scope,
+          size: size
+        })
+      }
+
+    }
   }
 
 })
